426790ecb2406e085a872fc7b9bed462aff83d09
sharif <shorif2000@gmail.com>
2018-01-08 23:09:23 +0000

    mod4 testing

--- module4/truffle.js
+++ module4/truffle.js
@@ -4,6 +4,16 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    "net42": {
+      host: "localhost",
+      port: 8545,
+      network_id: 42
+    },
+    "ropsten": {
+      host: "localhost",
+      port: 8545,
+      network_id: 3
     }
   }
 };
