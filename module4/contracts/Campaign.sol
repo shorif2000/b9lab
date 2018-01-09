88ee36b659d34b808cc6186eddc61042d21ccea9
sharif <shorif2000@gmail.com>
2018-01-09 19:49:18 +0000

    revert file to original

--- module4/contracts/Campaign.sol
+++ module4/contracts/Campaign.sol
@@ -1,75 +1,88 @@
-// Splitter
+// Contract Factory Pactice
 
 pragma solidity ^0.4.6;
 
-contract Splitter {
+contract Campaign {
     
-    address public bob; 
-    address public carol;
-    uint public bobAmount;
-    uint public carolAmount;
-    
-    struct SplitterStruct {
-        address payee;
+    address public owner;
+    uint    public deadline; //block number
+    uint    public goal; //wei
+    uint    public fundsRaised;
+
+    struct FunderStruct {
+        address funder;
         uint amount;
     }
     
-    SplitterStruct[] public splitStructs;
-	
-	mapping(address => uint) public recipientBalances;
-	//$recipientBalances[$address] = $resultasuint
-	
-	
-    function Splitter(address bobAddress, address carolAddress)
-        public
-    {
-        require(bobAddress != 0x0);
-        require(carolAddress != 0x0);
-        require(bobAddress != carolAddress);
-        bob = bobAddress;
-        carol = carolAddress;
+    event LogContribution(address sender, uint amount);
+    event LogRefund(address funder, uint amount);
+    event LogWithdrawal(address beneficiary, uint amount);
+
+    FunderStruct[] public funderStructs;
+
+    function Campaign(uint campaignDuration, uint campaignGoal){
+        owner = msg.sender;
+        deadline = block.number + campaignDuration;
+        goal = campaignGoal;
     }
     
-    function split() 
+    function isSuccess()
+        public
+        constant
+        returns(bool isIndeed)
+    {
+        return(fundsRaised >= goal);
+    }
+
+    function hasFailed()
+        public
+        constant
+        returns(bool hasIndeed)
+    {
+        return(fundsRaised < goal && block.number > deadline);
+    }
+
+    function contribute() 
         public 
         payable 
         returns(bool success)
     {
-        require(msg.value > 0);
-		require(msg.value%2 == 0) ;
-		
-		recipientBalances[bob] += msg.value/2;
-		//log here
-		recipientBalances[carol] += msg.value/2;
-		//log here
-		
-        //bob.transfer(msg.value/2);
-        //carol.transfer(msg.value/2);
+        if(msg.value==0) throw;
+        fundsRaised += msg.value;
+        FunderStruct memory newFunder;
+        newFunder.funder = msg.sender;
+        newFunder.amount = msg.value;
+        funderStructs.push(newFunder);
+        LogContribution(msg.sender, msg.value);
+        return true;
+    }
+    
+    function withdrawFunds() 
+        public 
+        returns (bool success)
+    {
+        if(msg.sender != owner) throw;
+        if(!isSuccess()) throw;
+        uint amount = this.balance;
+        if(!owner.send(amount)) throw;
+        LogWithdrawal(owner, this.balance);
+        return true;
+    }
+    
+    function sendRefunds() 
+        public 
+        returns (bool success)
+    {
+        if(msg.sender != owner) throw;
+        if(!hasFailed()) throw;
 
+        uint funderCount = funderStructs.length;
+        for(uint i=0; i<funderCount; i++){
+            if(!funderStructs[i].funder.send(funderStructs[i].amount)) {
+                
+            }
+            LogRefund(funderStructs[i].funder,funderStructs[i].amount);
+        }
         return true;
     }
-    
-    function withdraw()
-        public
-       // payable
-        returns(bool success)
-    {
-        // check if balance in mapping
-        require(recipientBalances[msg.sender] > 0 );//if(!recipientBalances[msg.sender]) throw;
-        // check sender exists
-        // reset balance to 0
-        // log event
-        msg.sender.transfer(recipientBalances[msg.sender]);
-        
-        return true;
-    }
-    
-    function checkBalance()
-        public
-        constant
-		returns(uint amount)
-    {
-        return recipientBalances[msg.sender];
-    }
-        
 }
