1534f8be44292a70b4de5c7303e184e91ab392e2
sharif <shorif2000@gmail.com>
2018-01-09 20:39:06 +0000

    added test unit

--- small_projects/module4/splitter/contracts/Splitter.sol
+++ small_projects/module4/splitter/contracts/Splitter.sol
@@ -4,28 +4,29 @@ pragma solidity ^0.4.6;
 
 contract Splitter {
     
+    address public alice;
     address public bob; 
     address public carol;
-    uint public bobAmount;
-    uint public carolAmount;
     
     struct SplitterStruct {
         address payee;
         uint amount;
     }
     
-    SplitterStruct[] public splitStructs;
-	
-	mapping(address => uint) public recipientBalances;
-	//$recipientBalances[$address] = $resultasuint
-	
-	
-    function Splitter(address bobAddress, address carolAddress)
+    mapping(address => uint) public recipientBalances;
+    //$recipientBalances[$address] = $resultasuint
+    
+    
+    function Splitter(address aliceAddress, address bobAddress, address carolAddress)
         public
     {
+        require(aliceAddress != 0x0);
         require(bobAddress != 0x0);
         require(carolAddress != 0x0);
+        require(aliceAddress != bobAddress);
+        require(aliceAddress != carolAddress);
         require(bobAddress != carolAddress);
+        alice = aliceAddress;
         bob = bobAddress;
         carol = carolAddress;
     }
@@ -35,41 +36,28 @@ contract Splitter {
         payable 
         returns(bool success)
     {
+        require(msg.sender == alice);
         require(msg.value > 0);
-		require(msg.value%2 == 0) ;
-		
-		recipientBalances[bob] += msg.value/2;
-		//log here
-		recipientBalances[carol] += msg.value/2;
-		//log here
-		
-        //bob.transfer(msg.value/2);
-        //carol.transfer(msg.value/2);
-
+        require(msg.value%2 == 0) ;
+        
+        recipientBalances[bob] += msg.value/2;
+        //log here
+        recipientBalances[carol] += msg.value/2;
+        //log here
+        
         return true;
     }
     
     function withdraw()
         public
-       // payable
         returns(bool success)
     {
-        // check if balance in mapping
+        // check if balance in mapping and check sender exists
         require(recipientBalances[msg.sender] > 0 );//if(!recipientBalances[msg.sender]) throw;
-        // check sender exists
-        // reset balance to 0
+        recipientBalances[msg.sender] = 0;
         // log event
         msg.sender.transfer(recipientBalances[msg.sender]);
         
         return true;
     }
-    
-    function checkBalance()
-        public
-        constant
-		returns(uint amount)
-    {
-        return recipientBalances[msg.sender];
-    }
-        
 }
--- small_projects/module4/splitter/test/splitter.js
+++ small_projects/module4/splitter/test/splitter.js
@@ -1,5 +1,28 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
 contract('Splitter', function(accounts) {
-  
+
+	var contract;
+	var alice = accounts[0];
+	var bob = accounts[1];  
+  	var carol = accounts[2];
+
+	beforeEach(function(){
+	    return Splitter.new(alice, bob, carol)
+	    .then(function(instance)  {
+	     	contract = instance;	      
+	    });
+	});
+
+	it("should check split function", function() {
+		var bobBeforeBalance = contract.recipientBalances[bob];
+		var carolBeforeBalance = contract.recipientBalances[carol];
+		return contract.split({from: alice, value: 2})
+		.then(function(_recipientBalances) {
+			assert(bobBeforeBalance > this.recipientBalances[bob],"Bob balance " + bobBeforeBalance + "is same " + this.recipientBalances[bob]);
+			assert.greaterThan(carolBeforeBalance,_recipientBalances[carol],"Carol balance is same");
+			return true;
+		});
+	});
+
 });
