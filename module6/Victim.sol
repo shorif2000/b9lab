0a4c1028a22b389aaa404fd73689904766e00676
sharif <shorif2000@gmail.com>
2018-01-20 20:03:59 +0000

    init

--- module6/Victim.sol
+++ module6/Victim.sol
@@ -1,1 +1,22 @@
+pragma solidity ^0.4.6;
 
+contract Victim{
+    uint public owedToAttacker;
+    
+    function Victim()
+    {
+        owedToAttacker = 11;
+    }
+    
+    function withdraw()
+    {
+        if(!msg.sender.send(owedToAttacker)) throw;
+        owedToAttacker = 0;
+    }
+    
+    function deposit() 
+        payable
+    {
+        
+    }
+}
